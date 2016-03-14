282b56fed9f6326bd3afdadeedf50282f33585e1
Brian Nash <bcnjr5@gmail.com>
2016-03-14 09:13:01 -0400

    Reformatted.

--- bconv.c
+++ bconv.c
@@ -16,16 +16,16 @@ static un atoi(const char *str, const uint8_t base) {
     while (*str != '\0') {
         value *= base;
         switch (*str) {
-          case '0': value += 0; break;
-          case '1': value += 1; break;
-          case '2': value += 2; break;
-          case '3': value += 3; break;
-          case '4': value += 4; break;
-          case '5': value += 5; break;
-          case '6': value += 6; break;
-          case '7': value += 7; break;
-          case '8': value += 8; break;
-          case '9': value += 9; break;
+          case '0':           value +=  0; break;
+          case '1':           value +=  1; break;
+          case '2':           value +=  2; break;
+          case '3':           value +=  3; break;
+          case '4':           value +=  4; break;
+          case '5':           value +=  5; break;
+          case '6':           value +=  6; break;
+          case '7':           value +=  7; break;
+          case '8':           value +=  8; break;
+          case '9':           value +=  9; break;
           case 'a': case 'A': value += 10; break;
           case 'b': case 'B': value += 11; break;
           case 'c': case 'C': value += 12; break;
